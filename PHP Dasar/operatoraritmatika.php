410e6947380681632a10f82b841e7bd0079e65c1
Reiza Suryadisastra <80139910+reizas2702@users.noreply.github.com>
2022-11-25 14:15:05 +0800

    Create operatoraritmatika.php

---
+++ PHP Dasar/operatoraritmatika.php
@@ -0,0 +1,19 @@
+    <!-- OPERATOR ARITMATIKA
+    +variable => positif 
+    -$variable => negatif
+    $variable + $variable => penambahan
+    $variable - $variable => pengurangan
+    $variable * $variable => perkalian
+    $variable / $variable => pembagian
+    $variable % $variable => sisa bagi
+    $variable ** $variable => pangkat -->
+
+<?php
+
+$a = 10;
+$b = 20;
+$variable = $a + $b; 
+var_dump ($a + $b);
+
+$variableNegative = -$variable;
+var_dump($variableNegative);

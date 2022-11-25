39ada05b77ce5a3a77eec6d000860fbe99d996d4
Reiza Suryadisastra <80139910+reizas2702@users.noreply.github.com>
2022-11-25 14:16:22 +0800

    Create operatorpembandingan.php

---
+++ PHP Dasar/operatorpembandingan.php
@@ -0,0 +1,17 @@
+<!-- Operator perbandingan
+-digunakan utk membandingkan dua buah value 
+-hasil dari operator perbandingan adalah boolean, true jika perbandingannya benar, dan false jika perbandingannya salah 
+
+$a==$b (sama dengan) -> true jika $a sama dengan $b setelah dilakukan konversi tipe data
+$a===$b (identik) -> true jika $a sama dengan $b dan memiliki tipe data yang sama setelah dilakukan konversi tipe data
+$a!=$b (tidak sama dengan) -> true jika $a tidak sama dengan $b dan memiliki tipe data yang sama setelah dilakukan konversi tipe data
+$a<>$b (tidak sama dengan) -> true jika $a tidak sama dengan $b dan memiliki tipe data yang sama setelah dilakukan konversi tipe data
+$a!==$b (tidak identik) -> true jika $a tidak sama dengan $b atau tidak sama tipe data 
+$a<$b (kurang dari) -> true jika $a kurang dari $b 
+$a<=$b (kurang dari atau sama dengan) -> true jika $a kurang dari atau sama dengan $b 
+$a>$b (lebih dari) -> true jika $a lebih dari $b 
+$a>=$b (lebih dari atau sama dengan) -> true jika $a lebih dari atau sama dengan $b -->
+
+<?php
+var_dump("10" === 10);
+

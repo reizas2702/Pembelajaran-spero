0c03f03785a21b82ae527c10c180ec3f335e0383
Reiza Suryadisastra <80139910+reizas2702@users.noreply.github.com>
2022-11-25 14:09:39 +0800

    Create tipedataboolean.php

---
+++ PHP Dasar/tipedataboolean.php
@@ -0,0 +1,13 @@
+<!-- Tipe data boolean tipe data yg paling sederhana dengan dua nilai yaitu benar(true) atau salah(false). nilai salah benar dipresentasikan case insensitive, mau true dan false nya huruf capital atau tidak, ga masalah   -->
+
+
+<?php
+echo "Benar :";
+var_dump(TruE);
+echo "Salah :";
+var_dump(fAlse);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
